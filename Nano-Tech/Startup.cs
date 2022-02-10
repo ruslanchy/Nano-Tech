a2ba4c19f4a5b204926ca7b252fcb1fe66c1da7c
ruslanchy <180204058@aust.edu>
2022-02-11 02:22:56 +0600

    Add project files.

---
+++ Nano-Tech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nano_Tech.Startup))]
+namespace Nano_Tech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
